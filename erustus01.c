36d0433616b4d9cd2cb8d563bddb9894ecb03524
erustus07 <erustusndegwa08@gmail.com>
2025-01-19 19:53:08 +0300

    Add C program for voting eligibility check

---
+++ erustus01.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <string.h>
+// getting users name 
+int main() {
+    char name[50];
+    int age;
+printf("Please enter your name: ");
+    fgets(name, sizeof(name), stdin);
+
+name[strcspn(name, "\n")] = 0;
+// age
+printf("Please enter your age: ");
+scanf ("%d", &age);
+//  cheking if the user is eligible to vote
+if (age >= 18) {
+    printf("Hello %s, you are eligible to vote!\n", name);
+    } else {
+    printf("Hello %s, you are not eligible to vote.\n", name);
+    
+}
+
+}
